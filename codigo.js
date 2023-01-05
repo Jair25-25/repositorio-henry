fb46673b328f68127ac8f1793b430ad9f1fde430
Jair25 <jairtronik@gmail.com>
2023-01-04 19:15:35 -0500

    Home work henry

---
+++ codigo.js
@@ -0,0 +1,24 @@
+dineroCofla = prompt("cuanto dinero tienes cofla?");
+
+
+if (dineroCofla > 0.6 && dineroCofla > 1) {
+    alert ("comprate el helado de agua")
+}
+
+else if (dineroCofla >= 1 && dineroCofla < 1.6) {
+alert("comprate el helado de crema")
+}
+else if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
+    alert("comprate el helado de Heladix")
+    }
+    else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
+        alert("comprate el helado de heladovich")
+        }
+        else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
+            alert("comprate el helado de Helardo")
+            }
+            else if (dineroCofla >= 2.9) {
+            alert ("helado con confites o el pote de 1/4kg") 
+                    } else  {
+                alert("lo siento, no te alcansa para el Helado")
+                }
